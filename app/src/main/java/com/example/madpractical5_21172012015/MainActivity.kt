8c10d85f7d71b8636a6b3d3d724f7b4aa154b7db
malay <malaykumarpatel18@gnu.ac.in>
2022-10-09 22:33:02 +0530

    Initial commit

---
+++ app/src/main/java/com/example/madpractical5_21172012015/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.madpractical5_21172012015
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.madpractical5_21172012015.MediaService
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var playbtn : FloatingActionButton
+    private lateinit var stopbtn : FloatingActionButton
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        playbtn = findViewById(R.id.btn_play)
+        playbtn.setOnClickListener(View.OnClickListener {
+            play()
+        })
+
+        stopbtn = findViewById(R.id.btn_stop)
+        stopbtn.setOnClickListener(View.OnClickListener {
+            stop()
+        })
+    }
+
+    private fun play(){
+        Intent(applicationContext, MediaService::class.java).putExtra(MediaService.DATA_KEY,MediaService.DATA_VALUE).apply { startService(this) }
+    }
+    private fun stop(){
+        Intent(applicationContext,MediaService::class.java).apply { stopService(this) }
+    }
+}
